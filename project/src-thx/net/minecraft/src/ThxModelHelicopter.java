a0026812980f04015710668da0d009677ce8ac43
Theoxylo <theoxylo@yahoo.com>
2011-05-17 11:26:42 -0700

    tweaking helicopter model geometry

--- project/src-thx/net/minecraft/src/ThxModelHelicopter.java
+++ project/src-thx/net/minecraft/src/ThxModelHelicopter.java
@@ -10,67 +10,56 @@ public class ThxModelHelicopter extends ModelBase
     float SPIN_UP_TIME = 300f;
     float timeSpun = 0f;
 
-    /*
-     * public ModelRenderer box;
-     * 
-     * int textureOffsetX = 0; int textureOffsetY = 0;
-     * 
-     * public ThxModelHelicopter() { float length = 56f; float width = 24f;
-     * float height = 32f; box = new ModelRenderer(textureOffsetX,
-     * textureOffsetY); box.addBox(-length/2f, -height/2f, -width/2f,
-     * (int)length, (int)height, (int)width, 0.0f); box.setPosition(0f, 0f, 0f);
-     * //box.rotateAngleX = 1.570796F; }
-     * 
-     * @Override public void render(float f, float f1, float f2, float f3, float
-     * f4, float f5) { box.render(f5); }
-     */
-
     public ThxModelHelicopter()
     {
         ENABLE_ROTOR = ThxConfig.getBoolProperty("enable_rotor");
         
-        float scale = 0f;
+        // dimensions -- adjusted and reused when adding each model box
+        float length = 0f;
+        float width  = 0f;
+        float height = 0f;
+        
         float xOff = 4f;
 
-        boatSides = new ModelRenderer[5];
-        boatSides[0] = new ModelRenderer(0, 8);
-        boatSides[1] = new ModelRenderer(0, 0);
-        boatSides[2] = new ModelRenderer(0, 0);
-        boatSides[3] = new ModelRenderer(0, 0);
-        boatSides[4] = new ModelRenderer(0, 0);
+        boxes = new ModelRenderer[5];
+        boxes[0] = new ModelRenderer(0, 8);
+        boxes[1] = new ModelRenderer(0, 0);
+        boxes[2] = new ModelRenderer(0, 0);
+        boxes[3] = new ModelRenderer(0, 0);
+        boxes[4] = new ModelRenderer(0, 0);
         
         byte byte0 = 24;
         byte byte1 = 6;
         byte byte2 = 20;
         byte byte3 = 4;
         
-        boatSides[0].addBox(-byte0 / 2, -byte2 / 2 + 2, -3F, byte0, byte2 - 4, 4, scale);
-        boatSides[0].setPosition(0.0F, 0 + byte3, 0.0F);
-        //boatSides[0].addBox(-24f / 2f, -20f / 2f + 2, -3F, 24, 20 - 4, 8, 0.0F);
-        //boatSides[0].setPosition(0f, 8f, 0f);
-        boatSides[0].rotateAngleX = 1.570796F;
+        boxes[0].addBox(-byte0 / 2, -byte2 / 2 + 2, -3F, byte0, byte2 - 4, 4);
+        boxes[0].setPosition(0.0F, 0 + byte3, 0.0F);
+        //boxes[0].addBox(-24f / 2f, -20f / 2f + 2, -3F, 24, 20 - 4, 8, 0.0F);
+        //boxes[0].setPosition(0f, 8f, 0f);
+        boxes[0].rotateAngleX = 1.570796F;
         
-        boatSides[1].addBox(-byte0 / 2 + 2, -byte1 - 1, -1F, byte0 - 4, byte1, 2, scale);
-        boatSides[1].setPosition(-byte0 / 2 + 1, 0 + byte3, 0.0F);
-        boatSides[1].rotateAngleY = 4.712389F;
+        boxes[1].addBox(-byte0 / 2 + 2, -byte1 - 1, -1F, byte0 - 4, byte1, 2);
+        boxes[1].setPosition(-byte0 / 2 + 1, 0 + byte3, 0.0F);
+        boxes[1].rotateAngleY = 4.712389F;
         
-        boatSides[2].addBox(-byte0 / 2 + 2, -byte1 - 1, -1F, byte0 - 4, byte1, 2, scale);
-        boatSides[2].setPosition(byte0 / 2 - 1, 0 + byte3, 0.0F);
-        boatSides[2].rotateAngleY = 1.570796F;
+        boxes[2].addBox(-byte0 / 2 + 2, -byte1 - 1, -1F, byte0 - 4, byte1, 2);
+        boxes[2].setPosition(byte0 / 2 - 1, 0 + byte3, 0.0F);
+        boxes[2].rotateAngleY = 1.570796F;
         
-        boatSides[3].addBox(-byte0 / 2 + 2, -byte1 - 1, -1F, byte0 - 4, byte1, 2, scale);
-        boatSides[3].setPosition(0.0F, 0 + byte3, -byte2 / 2 + 1);
-        boatSides[3].rotateAngleY = 3.141593F;
+        boxes[3].addBox(-byte0 / 2 + 2, -byte1 - 1, -1F, byte0 - 4, byte1, 2);
+        boxes[3].setPosition(0.0F, 0 + byte3, -byte2 / 2 + 1);
+        boxes[3].rotateAngleY = 3.141593F;
         
-        boatSides[4].addBox(-byte0 / 2 + 2, -byte1 - 1, -1F, byte0 - 4, byte1, 2, scale);
-        boatSides[4].setPosition(0.0F, 0 + byte3, byte2 / 2 - 1);
+        boxes[4].addBox(-byte0 / 2 + 2, -byte1 - 1, -1F, byte0 - 4, byte1, 2);
+        boxes[4].setPosition(0.0F, 0 + byte3, byte2 / 2 - 1);
 
         // rotor
-        float length = 2f;
-        float width = 64f;
-        float height = 1f;
+        length = 2f;
+        width = 64f;
+        height = 1f;
         rotor1 = new ModelRenderer(0, 0);
-        rotor1.addBox(-length / 2f, -height / 2f, -width / 2f, (int) length, (int) height, (int) width, 0.0f);
+        rotor1.addBox(-length / 2f, -height / 2f, -width / 2f, (int) length, (int) height, (int) width);
         rotor1.setPosition(0f, -24f, 0f);
         rotor1.rotateAngleY = 1.75f; // start off axis for realism
 
@@ -79,7 +68,7 @@ public class ThxModelHelicopter extends ModelBase
         width = 4f;
         height = 24f;
         rotor2 = new ModelRenderer(0, 0);
-        rotor2.addBox(-length / 2f, -height / 2f, -width / 2f, (int) length, (int) height, (int) width, 0.0f);
+        rotor2.addBox(-length / 2f, -height / 2f, -width / 2f, (int) length, (int) height, (int) width);
         rotor2.setPosition(14f, -9f, 0f);
 
         // rotor horizontal support
@@ -87,7 +76,7 @@ public class ThxModelHelicopter extends ModelBase
         width = 4f;
         height = 2f;
         rotor3 = new ModelRenderer(0, 0);
-        rotor3.addBox(-length / 2f, -height / 2f, -width / 2f, (int) length, (int) height, (int) width, 0.0f);
+        rotor3.addBox(-length / 2f, -height / 2f, -width / 2f, (int) length, (int) height, (int) width);
         rotor3.setPosition(14f, -22, 0f);
 
         // cockpit1 vertical support
@@ -95,7 +84,7 @@ public class ThxModelHelicopter extends ModelBase
         width = 1f;
         height = 13f;
         cockpit1 = new ModelRenderer(0, 0);
-        cockpit1.addBox(-length / 2f, -height / 2f, -width / 2f, (int) length, (int) height, (int) width, 0.0f);
+        cockpit1.addBox(-length / 2f, -height / 2f, -width / 2f, (int) length, (int) height, (int) width);
         cockpit1.setPosition(-10f, -9f, 9f);
         
         // cockpit2 vertical support
@@ -103,7 +92,7 @@ public class ThxModelHelicopter extends ModelBase
         width = 1f;
         height = 13f;
         cockpit2 = new ModelRenderer(0, 0);
-        cockpit2.addBox(-length / 2f, -height / 2f, -width / 2f, (int) length, (int) height, (int) width, 0.0f);
+        cockpit2.addBox(-length / 2f, -height / 2f, -width / 2f, (int) length, (int) height, (int) width);
         cockpit2.setPosition(-10f, -9f, -9f);
         
         // cockpit3 horizontal support
@@ -111,7 +100,7 @@ public class ThxModelHelicopter extends ModelBase
         width = 19f;
         height = 1f;
         cockpit3 = new ModelRenderer(0, 0);
-        cockpit3.addBox(-length / 2f, -height / 2f, -width / 2f, (int) length, (int) height, (int) width, 0.0f);
+        cockpit3.addBox(-length / 2f, -height / 2f, -width / 2f, (int) length, (int) height, (int) width);
         cockpit3.setPosition(-10f, -16f, 0f);
     }
 
@@ -120,7 +109,7 @@ public class ThxModelHelicopter extends ModelBase
     {
         for(int i = 0; i < 5; i++)
         {
-            boatSides[i].render(f5);
+            boxes[i].render(f5);
         }
         
         if (ENABLE_ROTOR)
@@ -181,5 +170,5 @@ public class ThxModelHelicopter extends ModelBase
     public ModelRenderer cockpit2;
     public ModelRenderer cockpit3;
 
-    public ModelRenderer boatSides[];
+    public ModelRenderer boxes[];
 }
