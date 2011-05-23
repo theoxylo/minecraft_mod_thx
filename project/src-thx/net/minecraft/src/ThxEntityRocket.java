09368dabf5cb5d30e972e062b262079523128249
Theoxylo <theoxylo@yahoo.com>
2011-05-23 13:20:52 -0700

    added new ThxEntityRocket entity class

---
+++ project/src-thx/net/minecraft/src/ThxEntityRocket.java
@@ -0,0 +1,301 @@
+package net.minecraft.src;
+
+import java.util.List;
+import java.util.Random;
+
+// Referenced classes of package net.minecraft.src:
+//            Entity, Vec3D, AxisAlignedBB, World, 
+//            Item, EntityPlayer, MathHelper, InventoryPlayer, 
+//            ItemStack, EntityLiving, NBTTagCompound, MovingObjectPosition, 
+//            EntityChicken
+
+public class ThxEntityRocket  extends Entity
+{
+
+    public ThxEntityRocket(World world)
+    {
+        super(world);
+        xTile = -1;
+        yTile = -1;
+        zTile = -1;
+        inTile = 0;
+        inGround = false;
+        shake = 0;
+        field_20049_i = 0;
+        setSize(0.25F, 0.25F);
+    }
+
+    protected void entityInit()
+    {
+    }
+
+    public boolean isInRangeToRenderDist(double d)
+    {
+        double d1 = boundingBox.getAverageEdgeLength() * 4D;
+        d1 *= 64D;
+        return d < d1 * d1;
+    }
+
+    public ThxEntityRocket(World world, EntityLiving entityliving)
+    {
+        super(world);
+        
+        xTile = -1;
+        yTile = -1;
+        zTile = -1;
+        inTile = 0;
+        inGround = false;
+        shake = 0;
+        field_20049_i = 0;
+        owner = entityliving;
+        setSize(0.25F, 0.25F);
+        setLocationAndAngles(entityliving.posX, entityliving.posY + (double)entityliving.getEyeHeight(), entityliving.posZ, entityliving.rotationYaw, entityliving.rotationPitch);
+        posX -= MathHelper.cos((rotationYaw / 180F) * 3.141593F) * 0.16F;
+        posY -= 0.10000000149011612D;
+        posZ -= MathHelper.sin((rotationYaw / 180F) * 3.141593F) * 0.16F;
+        setPosition(posX, posY, posZ);
+        yOffset = 0.0F;
+        float acceleration = .6f; //0.4F;
+        motionX = -MathHelper.sin((rotationYaw / 180F) * 3.141593F) * MathHelper.cos((rotationPitch / 180F) * 3.141593F) * acceleration;
+        motionZ = MathHelper.cos((rotationYaw / 180F) * 3.141593F) * MathHelper.cos((rotationPitch / 180F) * 3.141593F) * acceleration;
+        motionY = -MathHelper.sin((rotationPitch / 180F) * 3.141593F) * acceleration;
+        setEggHeading(motionX, motionY, motionZ, 1.5F, 1.0F);
+        
+        worldObj.playSoundAtEntity(this, "random.fizz", 1f, 1f);
+    }
+
+    public ThxEntityRocket(World world, double x, double y, double z)
+    {
+        super(world);
+        
+        xTile = -1;
+        yTile = -1;
+        zTile = -1;
+        inTile = 0;
+        inGround = false;
+        shake = 0;
+        field_20049_i = 0;
+        field_20050_h = 0;
+        setSize(0.25F, 0.25F);
+        setPosition(x, y, z);
+        yOffset = 0.0F;
+    }
+
+    public void setEggHeading(double d, double d1, double d2, float f, 
+            float f1)
+    {
+        float f2 = MathHelper.sqrt_double(d * d + d1 * d1 + d2 * d2);
+        d /= f2;
+        d1 /= f2;
+        d2 /= f2;
+        d += rand.nextGaussian() * 0.0074999998323619366D * (double)f1;
+        d1 += rand.nextGaussian() * 0.0074999998323619366D * (double)f1;
+        d2 += rand.nextGaussian() * 0.0074999998323619366D * (double)f1;
+        d *= f;
+        d1 *= f;
+        d2 *= f;
+        motionX = d;
+        motionY = d1;
+        motionZ = d2;
+        float f3 = MathHelper.sqrt_double(d * d + d2 * d2);
+        prevRotationYaw = rotationYaw = (float)((Math.atan2(d, d2) * 180D) / 3.1415927410125732D);
+        prevRotationPitch = rotationPitch = (float)((Math.atan2(d1, f3) * 180D) / 3.1415927410125732D);
+        field_20050_h = 0;
+    }
+
+    public void setVelocity(double d, double d1, double d2)
+    {
+        motionX = d;
+        motionY = d1;
+        motionZ = d2;
+        if(prevRotationPitch == 0.0F && prevRotationYaw == 0.0F)
+        {
+            float f = MathHelper.sqrt_double(d * d + d2 * d2);
+            prevRotationYaw = rotationYaw = (float)((Math.atan2(d, d2) * 180D) / 3.1415927410125732D);
+            prevRotationPitch = rotationPitch = (float)((Math.atan2(d1, f) * 180D) / 3.1415927410125732D);
+        }
+    }
+
+    public void onUpdate()
+    {
+        lastTickPosX = posX;
+        lastTickPosY = posY;
+        lastTickPosZ = posZ;
+        super.onUpdate();
+        if(shake > 0)
+        {
+            shake--;
+        }
+        if(inGround)
+        {
+            int i = worldObj.getBlockId(xTile, yTile, zTile);
+            if(i != inTile)
+            {
+                inGround = false;
+                motionX *= rand.nextFloat() * 0.2F;
+                motionY *= rand.nextFloat() * 0.2F;
+                motionZ *= rand.nextFloat() * 0.2F;
+                field_20050_h = 0;
+                field_20049_i = 0;
+            } else
+            {
+                field_20050_h++;
+                if(field_20050_h == 1200)
+                {
+                    setEntityDead();
+                    
+                    // out of range?
+		            worldObj.playSoundAtEntity(this, "random.pop", 1f, 1f);
+                }
+                return;
+            }
+        } else
+        {
+            field_20049_i++;
+        }
+        Vec3D vec3d = Vec3D.createVector(posX, posY, posZ);
+        Vec3D vec3d1 = Vec3D.createVector(posX + motionX, posY + motionY, posZ + motionZ);
+        MovingObjectPosition movingobjectposition = worldObj.rayTraceBlocks(vec3d, vec3d1);
+        /*
+        vec3d = Vec3D.createVector(posX, posY, posZ);
+        vec3d1 = Vec3D.createVector(posX + motionX, posY + motionY, posZ + motionZ);
+        */
+        if(movingobjectposition != null)
+        {
+            vec3d1 = Vec3D.createVector(movingobjectposition.hitVec.xCoord, movingobjectposition.hitVec.yCoord, movingobjectposition.hitVec.zCoord);
+        }
+        if(!worldObj.multiplayerWorld)
+        {
+            Entity entity = null;
+            List list = worldObj.getEntitiesWithinAABBExcludingEntity(this, boundingBox.addCoord(motionX, motionY, motionZ).expand(1.0D, 1.0D, 1.0D));
+            double d = 0.0D;
+            for(int i1 = 0; i1 < list.size(); i1++)
+            {
+                Entity entity1 = (Entity)list.get(i1);
+                if(!entity1.canBeCollidedWith() || entity1 == owner && field_20049_i < 50)
+                {
+                    continue;
+                }
+                float f4 = 0.3F;
+                AxisAlignedBB axisalignedbb = entity1.boundingBox.expand(f4, f4, f4);
+                MovingObjectPosition movingobjectposition1 = axisalignedbb.func_1169_a(vec3d, vec3d1);
+                if(movingobjectposition1 == null)
+                {
+                    continue;
+                }
+                double d1 = vec3d.distanceTo(movingobjectposition1.hitVec);
+                if(d1 < d || d == 0.0D)
+                {
+                    entity = entity1;
+                    d = d1;
+                }
+            }
+
+            if(entity != null)
+            {
+                movingobjectposition = new MovingObjectPosition(entity);
+            }
+        }
+        if(movingobjectposition != null)
+        {
+            // we hit an entity!
+            if(movingobjectposition.entityHit != null)
+            {
+                //if(!movingobjectposition.entityHit.attackEntityFrom(owner, 0));
+                movingobjectposition.entityHit.attackEntityFrom(owner, 1);
+            }
+            
+            worldObj.spawnParticle("flame", posX, posY, posZ, 0.0D, 0.0D, 0.0D);
+            
+            worldObj.playSoundAtEntity(this, "random.dr", 1f, 1f);
+
+            setEntityDead();
+        }
+        
+        posX += motionX;
+        posY += motionY;
+        posZ += motionZ;
+        float f = MathHelper.sqrt_double(motionX * motionX + motionZ * motionZ);
+        rotationYaw = (float)((Math.atan2(motionX, motionZ) * 180D) / 3.1415927410125732D);
+        for(rotationPitch = (float)((Math.atan2(motionY, f) * 180D) / 3.1415927410125732D); 
+        rotationPitch - prevRotationPitch < -180F; prevRotationPitch -= 360F) { }
+        for(; rotationPitch - prevRotationPitch >= 180F; prevRotationPitch += 360F) { }
+        for(; rotationYaw - prevRotationYaw < -180F; prevRotationYaw -= 360F) { }
+        for(; rotationYaw - prevRotationYaw >= 180F; prevRotationYaw += 360F) { }
+        rotationPitch = prevRotationPitch + (rotationPitch - prevRotationPitch) * 0.2F;
+        rotationYaw = prevRotationYaw + (rotationYaw - prevRotationYaw) * 0.2F;
+        float f1 = 0.99F;
+        if(func_27013_ag())
+        {
+            for(int l = 0; l < 4; l++)
+            {
+                float f3 = 0.25F;
+                worldObj.spawnParticle("bubble", posX - motionX * (double)f3, posY - motionY * (double)f3, posZ - motionZ * (double)f3, motionX, motionY, motionZ);
+            }
+
+            f1 = 0.8F;
+        }
+        motionX *= f1;
+        motionY *= f1;
+        motionZ *= f1;
+        
+        float gravity = 0.001f;
+        motionY -= gravity;
+        
+        setPosition(posX, posY, posZ);
+    }
+
+    public void writeEntityToNBT(NBTTagCompound nbttagcompound)
+    {
+        nbttagcompound.setShort("xTile", (short)xTile);
+        nbttagcompound.setShort("yTile", (short)yTile);
+        nbttagcompound.setShort("zTile", (short)zTile);
+        nbttagcompound.setByte("inTile", (byte)inTile);
+        nbttagcompound.setByte("shake", (byte)shake);
+        nbttagcompound.setByte("inGround", (byte)(inGround ? 1 : 0));
+    }
+
+    public void readEntityFromNBT(NBTTagCompound nbttagcompound)
+    {
+        xTile = nbttagcompound.getShort("xTile");
+        yTile = nbttagcompound.getShort("yTile");
+        zTile = nbttagcompound.getShort("zTile");
+        inTile = nbttagcompound.getByte("inTile") & 0xff;
+        shake = nbttagcompound.getByte("shake") & 0xff;
+        inGround = nbttagcompound.getByte("inGround") == 1;
+    }
+
+    // picked up by player upon contact?
+    /*
+    public void onCollideWithPlayer(EntityPlayer entityplayer)
+    {
+        if(inGround 
+                && owner == entityplayer 
+                && shake <= 0 
+                && entityplayer.inventory.addItemStackToInventory(new ItemStack(Item.arrow, 1)))
+        {
+            worldObj.playSoundAtEntity(this, "random.pop", 0.2F, ((rand.nextFloat() - rand.nextFloat()) * 0.7F + 1.0F) * 2.0F);
+            
+            entityplayer.onItemPickup(this, 1);
+            
+            setEntityDead();
+        }
+    }
+    */
+
+    public float getShadowSize()
+    {
+        return 0.0F;
+    }
+
+    private int xTile;
+    private int yTile;
+    private int zTile;
+    private int inTile;
+    private boolean inGround;
+    public int shake;
+    private EntityLiving owner;
+    private int field_20050_h;
+    private int field_20049_i;
+}
+
