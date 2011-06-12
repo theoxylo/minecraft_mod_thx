0adb4ddf4611e97fb10fcea980a2ce33f928081c
Theoxylo <theoxylo@yahoo.com>
2011-06-12 00:00:08 -0700

    fixed height-width swap

--- project/src-thx/net/minecraft/src/ThxModelMissile.java
+++ project/src-thx/net/minecraft/src/ThxModelMissile.java
@@ -11,7 +11,7 @@ public class ThxModelMissile extends ThxModel
         float y_height = 4f;
 
         missile = new ModelRenderer(0, 0);
-        missile.addBox(-x_length/2f, -z_width/2f, -y_height/2f, (int)x_length, (int)z_width, (int)y_height);
+        missile.addBox(-x_length/2f, -y_height/2f, -z_width/2f, (int)x_length, (int)y_height, (int)z_width);
         missile.setRotationPoint(0f, 0f, 0f);
     }
 
@@ -23,8 +23,9 @@ public class ThxModelMissile extends ThxModel
         if (!visible) return;
         
         // spiral
-        missile.rotateAngleX += deltaTime * 5f; // per sec
+        missile.rotateAngleX += deltaTime * 5f; // radians per sec
         if (missile.rotateAngleX > 2*PI) missile.rotateAngleX -= 2*PI;
+        
         missile.render(f5);
     }
 }
