7fd887c171179ac86c9690336f8f10280d227a61
Theoxylo <theoxylo@yahoo.com>
2011-10-18 23:55:02 -0700

    finished with texture mapping for helicopter model

--- project/src-thx/net/minecraft/src/ThxModelHelicopter.java
+++ project/src-thx/net/minecraft/src/ThxModelHelicopter.java
@@ -14,7 +14,7 @@ public class ThxModelHelicopter extends ThxModel
     float timeSpun = 0f;
 
     boolean ENABLE_ROTOR;
-    public ModelRenderer rotor1;
+    public ModelRenderer mainRotor;
     public ModelRenderer rotor2;
     public ModelRenderer rotor3;
     
@@ -56,7 +56,7 @@ public class ThxModelHelicopter extends ThxModel
 	        width = 2f;
 	        bottom = new ModelRenderer(this, 40, 22); // texture offset: 
 	        bottom.addBox(-length/2f, -height/2f, -width/2f, (int)length, (int)height, (int)width);
-	        bottom.rotationPointY = 2f; // halfed to adjust for scale 	        bottom.rotateAngleX = PI / 2f; // 90 deg roll left to lay flat
+	        bottom.rotationPointY = 2f; // halved to adjust for scale 	        bottom.rotateAngleX = PI / 2f; // 90 deg roll left to lay flat
         }
         
         frontWall:
@@ -103,71 +103,93 @@ public class ThxModelHelicopter extends ThxModel
 	        rightWall.rotateAngleY = PI; // flip 180 deg so decal is on outside
         }
         
-        // rotor
-        length = 64f;
-        height = 1f;
-        width = 2f;
-        rotor1 = new ModelRenderer(this, 0, 0);
-        rotor1.addBox(-length/2f, -height/2f, -width/2f, (int)length, (int)height, (int)width);
-        rotor1.setRotationPoint(6f, -24f, 0f);
-        rotor1.rotateAngleY = 1.75f; // start off axis for realism
+        mainRotor: //will be scaled x2
+        {
+	        length = 30f;
+	        height =  0f;
+	        width  =  1f;
+	        mainRotor = new ModelRenderer(this, 0, 0);
+	        mainRotor.addBox(-length/2f, -height/2f, -width/2f, (int)length, (int)height, (int)width);
+	        //mainRotor.setRotationPoint(6f, -24f, 0f);
+	        mainRotor.setRotationPoint(3f, -11.7f, 0f); // halved to adjust for scale, and a little subtracted 
+	        
+	        mainRotor.rotateAngleY = 1.75f; // start off axis for realism
+        }
         
-        // tail rotor
-        length = 16f;
-        height = 1f;
-        width  = 2f;
-        tailRotor = new ModelRenderer(this, 0, 0);
-        tailRotor.addBox(-length/2f, -height/2f, -width/2f, (int)length, (int)height, (int)width);
-        tailRotor.setRotationPoint(36f, -14f, 3f);
-        tailRotor.rotateAngleX = PI / 2f;
+        tailRotor:
+        {
+	        length = 16f;
+	        height = 2f;
+	        width  = 0f;
+	        tailRotor = new ModelRenderer(this, 8, 30);
+	        tailRotor.addBox(-length/2f, -height/2f, -width/2f, (int)length, (int)height, (int)width);
+	        tailRotor.setRotationPoint(34f, -14f, 1.4f);
+        }
 
-        // tail
-        length = 22f;
-        height = 4f;
-        width = 4f;
-        tail = new ModelRenderer(this, 0, 4);
-        tail.addBox(-length/2f, -height/2f, -width/2f, (int)length, (int)height, (int)width);
-        tail.setRotationPoint(27, -14, 0f);
+        tail: // will be scaled x2
+        {
+	        length = 10f;
+	        height =  2f;
+	        width  =  1f;
+	        tail = new ModelRenderer(this, 12, 2);
+	        tail.addBox(-length/2f, -height/2f, -width/2f, (int)length, (int)height, (int)width);
+	        //tail.setRotationPoint(26, -14, 0f);
+	        tail.setRotationPoint(13f, -7f, 0f); // adjusted for scale
+        }
 
-        // rotor vertical support
-        length = 4f;
-        height = 24f;
-        width = 4f;
-        rotor2 = new ModelRenderer(this, 0, 4);
-        rotor2.addBox(-length/2f, -height/2f, -width/2f, (int)length, (int)height, (int)width);
-        rotor2.setRotationPoint(14f, -9f, 0f);
+        rotorVerticalSupport:
+        {
+	        length = 4f;
+	        height = 22f;
+	        width = 3f;
+	        rotor2 = new ModelRenderer(this, 0, 3);
+	        rotor2.addBox(-length/2f, -height/2f, -width/2f, (int)length, (int)height, (int)width);
+	        rotor2.setRotationPoint(14f, -10f, 0f);
+        }
 
-        // rotor horizontal support
-        length = 12;
-        height = 2f;
-        width = 4f;
-        rotor3 = new ModelRenderer(this, 0, 4);
-        rotor3.addBox(-length/2f, -height/2f, -width/2f, (int)length, (int)height, (int)width);
-        rotor3.setRotationPoint(10, -22, 0f);
+        rotorHorizontalSupport:
+        {
+	        length = 12;
+	        height = 2f;
+	        width = 3f;
+	        rotor3 = new ModelRenderer(this, 34, 1);
+	        rotor3.addBox(-length/2f, -height/2f, -width/2f, (int)length, (int)height, (int)width);
+	        rotor3.setRotationPoint(10, -22, 0f);
+        }
 
         // cockpit1 vertical support
-        length = 1f;
-        height = 13f;
-        width = 1f;
-        cockpit1 = new ModelRenderer(this, 0, 4);
-        cockpit1.addBox(-length/2f, -height/2f, -width/2f, (int)length, (int)height, (int)width);
-        cockpit1.setRotationPoint(-10f, -9f, 9f);
+        cockpit1:
+        {
+	        length = 1f;
+	        height = 13f;
+	        width = 1f;
+	        cockpit1 = new ModelRenderer(this, 15, 7);
+	        cockpit1.addBox(-length/2f, -height/2f, -width/2f, (int)length, (int)height, (int)width);
+	        cockpit1.setRotationPoint(-10f, -9f, 9f);
+        }
         
         // cockpit2 vertical support
-        length = 1f;
-        height = 13f;
-        width = 1f;
-        cockpit2 = new ModelRenderer(this, 0, 4);
-        cockpit2.addBox(-length/2f, -height/2f, -width/2f, (int)length, (int)height, (int)width);
-        cockpit2.setRotationPoint(-10f, -9f, -9f);
+        cockpit2:
+        {
+	        length = 1f;
+	        height = 13f;
+	        width = 1f;
+	        cockpit2 = new ModelRenderer(this, 15, 7);
+	        cockpit2.addBox(-length/2f, -height/2f, -width/2f, (int)length, (int)height, (int)width);
+	        cockpit2.setRotationPoint(-10f, -9f, -9f);
+        }
         
         // cockpit3 horizontal support
-        length = 1f;
-        height = 1f;
-        width = 19f;
-        cockpit3 = new ModelRenderer(this, 0, 4);
-        cockpit3.addBox(-length/2f, -height/2f, -width/2f, (int)length, (int)height, (int)width);
-        cockpit3.setRotationPoint(-10f, -16f, 0f);
+        cockpit3:
+        {
+	        length =  1f;
+	        height = 19f;
+	        width  =  1f;
+	        cockpit3 = new ModelRenderer(this, 15, 7);
+	        cockpit3.addBox(-length/2f, -height/2f, -width/2f, (int)length, (int)height, (int)width);
+	        cockpit3.setRotationPoint(-10f, -16f, 0f);
+	        cockpit3.rotateAngleX = PI / 2f;
+        }
     }
 
     public void render()
@@ -194,7 +216,7 @@ public class ThxModelHelicopter extends ThxModel
         rotor2.render(scale);
         rotor3.render(scale);
         
-        tail.render(scale);
+        tail.render(scale * 2f);
         
         if (ENABLE_ROTOR)
         {
@@ -204,19 +226,19 @@ public class ThxModelHelicopter extends ThxModel
                 {
                     timeSpun += deltaTime * 3f; // spin up faster than spin down
                     
-                    rotor1.rotateAngleY    += deltaTime * MAX_ROTOR_SPEED * rotorSpeed * timeSpun / SPIN_UP_TIME;
+                    mainRotor.rotateAngleY += deltaTime * MAX_ROTOR_SPEED * rotorSpeed * timeSpun / SPIN_UP_TIME;
 			        tailRotor.rotateAngleZ -= deltaTime * MAX_ROTOR_SPEED * timeSpun / SPIN_UP_TIME; // not linked to throttle
                 }
                 else
                 {
-                    rotor1.rotateAngleY    += deltaTime * MAX_ROTOR_SPEED * rotorSpeed;
+                    mainRotor.rotateAngleY += deltaTime * MAX_ROTOR_SPEED * rotorSpeed;
                     tailRotor.rotateAngleZ -= deltaTime * MAX_ROTOR_SPEED;
                 }
                 
-		        if (rotor1.rotateAngleY > 2*PI) rotor1.rotateAngleY -= 2*PI;
-                rotor1.render(scale);
-                rotor1.rotateAngleY += 1.5707f; // add second blade perp
-                rotor1.render(scale);
+		        if (mainRotor.rotateAngleY > 2*PI) mainRotor.rotateAngleY -= 2*PI;
+                mainRotor.render(scale * 2f);
+                mainRotor.rotateAngleY += 1.5707f; // add second blade perp
+                mainRotor.render(scale * 2f);
 
 		        if (tailRotor.rotateAngleZ < 2*PI) tailRotor.rotateAngleZ += 2*PI;
 		        tailRotor.render(scale);
@@ -231,22 +253,22 @@ public class ThxModelHelicopter extends ThxModel
                 {
                     timeSpun -= deltaTime;
                     
-                    rotor1.rotateAngleY += deltaTime * MAX_ROTOR_SPEED * (1 - MathHelper.cos(timeSpun / SPIN_UP_TIME));
+                    mainRotor.rotateAngleY += deltaTime * MAX_ROTOR_SPEED * (1 - MathHelper.cos(timeSpun / SPIN_UP_TIME));
                     tailRotor.rotateAngleZ += deltaTime * MAX_ROTOR_SPEED * (1 - MathHelper.cos(timeSpun / SPIN_UP_TIME));
                     
                     // remember stopping position
-	                lastRotorRad = rotor1.rotateAngleY;
+	                lastRotorRad = mainRotor.rotateAngleY;
 	                lastTailRotorRad = tailRotor.rotateAngleZ;
                 }
                 else
                 {
-                    rotor1.rotateAngleY = lastRotorRad;
+                    mainRotor.rotateAngleY = lastRotorRad;
                     tailRotor.rotateAngleZ = lastTailRotorRad;
                 }
                 
-                rotor1.render(scale);
-                rotor1.rotateAngleY += 1.5707f; // add second blade perp
-                rotor1.render(scale);
+                mainRotor.render(scale * 2f);
+                mainRotor.rotateAngleY += 1.5707f; // add second blade perp
+                mainRotor.render(scale * 2f);
                 
 		        tailRotor.render(scale);
                 tailRotor.rotateAngleZ -= 1.5707f; // add second blade perp
@@ -256,10 +278,10 @@ public class ThxModelHelicopter extends ThxModel
         else
         {
             // show fixed rotor by rendering twice
-            rotor1.rotateAngleY = 0.7854f;
-            rotor1.render(scale);
-            rotor1.rotateAngleY += 1.5707f;
-            rotor1.render(scale);
+            mainRotor.rotateAngleY = 0.7854f;
+            mainRotor.render(scale * 2f);
+            mainRotor.rotateAngleY += 1.5707f;
+            mainRotor.render(scale * 2f);
             
             tailRotor.rotateAngleZ = 0.7854f;
             tailRotor.render(scale);
