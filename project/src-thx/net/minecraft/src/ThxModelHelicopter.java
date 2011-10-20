6aa2e39020ab154f93604b7a6cf55d768cf37b8c
Theoxylo <theoxylo@yahoo.com>
2011-10-20 00:39:08 -0700

    hi-rez helicopter texture

--- project/src-thx/net/minecraft/src/ThxModelHelicopter.java
+++ project/src-thx/net/minecraft/src/ThxModelHelicopter.java
@@ -46,17 +46,17 @@ public class ThxModelHelicopter extends ThxModel
 
         bottom:
         {
-	        // 1. to save texture space, we minimize width (Z value) by rotating. so 20x4x16 -> 20x16x4
-	        // 2. then we have to rotate piece back into place. rotate about x axis (roll) to swap y and z
-	        // 3. make half size and then scale later to save texture area (but half rez too) -> 10x8x2  
+	        // 1. to save texture space, create piece in upright orientation. so 20x4x16 -> 16x20x4
+	        // 2. then we have to rotate piece back into place. rotate about y, then x axis
+	        // 3. make half size and then scale later to save texture area (but half rez too) -> 8x10x2  
 	        // 4. also have to adjust ("reduce") position (rotationPt) since it moves when we scale
-	        // 5. but at least rotationPt is not affected by rotation (by definition), so no adjustment for that
-	        length = 10f;
-	        height = 8f;
-	        width = 2f;
-	        bottom = new ModelRenderer(this, 40, 22); // texture offset: 
+	        length =  8f;
+	        height = 10f;
+	        width  =  2f;
+	        bottom = new ModelRenderer(this, 0, 20); // texture offset: 
 	        bottom.addBox(-length/2f, -height/2f, -width/2f, (int)length, (int)height, (int)width);
-	        bottom.rotationPointY = 2f; // halved to adjust for scale 	        bottom.rotateAngleX = PI / 2f; // 90 deg roll left to lay flat
+	        bottom.rotationPointY = 2f; // halved to adjust for scale 	        bottom.rotateAngleY = PI / 2f; // 90 deg roll left to lay flat
+	        bottom.rotateAngleX = PI / 2f; // 90 deg roll left to lay flat
         }
         
         frontWall:
@@ -65,7 +65,7 @@ public class ThxModelHelicopter extends ThxModel
 	        length = 20f;
 	        height = 6f;
 	        width = 2f;
-	        frontWall = new ModelRenderer(this, 20, 14);
+	        frontWall = new ModelRenderer(this, 0, 4);
 	        frontWall.addBox(-length/2f, -height/2f, -width/2f, (int)length, (int)height, (int)width);
 	        frontWall.setRotationPoint(-11f, 0f, 0f); // placed 11 unit in front
 	        frontWall.rotateAngleY = PI * 1.5f; // 270 deg yaw
@@ -76,7 +76,7 @@ public class ThxModelHelicopter extends ThxModel
 	        length = 20f;
 	        height = 6f;
 	        width = 2f;
-	        backWall = new ModelRenderer(this, 20, 14);
+	        backWall = new ModelRenderer(this, 0, 4);
 	        backWall.addBox(-length/2f, -height/2f, -width/2f, (int)length, (int)height, (int)width);
 	        backWall.setRotationPoint(11, 0f, 0f);
 	        backWall.rotateAngleY = PI / 2f; // 90 deg yaw
@@ -87,7 +87,7 @@ public class ThxModelHelicopter extends ThxModel
 	        length = 20f;
 	        height = 6f;
 	        width = 2f;
-	        leftWall = new ModelRenderer(this, 20, 6);
+	        leftWall = new ModelRenderer(this, 0, 12);
 	        leftWall.addBox(-length/2f, -height/2f, -width/2f, (int)length, (int)height, (int)width);
 	        leftWall.setRotationPoint(0f, 0f, -9f);
         }
@@ -97,7 +97,7 @@ public class ThxModelHelicopter extends ThxModel
 	        length = 20f;
 	        height = 6f;
 	        width = 2f;
-	        rightWall = new ModelRenderer(this, 20, 6);
+	        rightWall = new ModelRenderer(this, 0, 12);
 	        rightWall.addBox(-length/2f, -height/2f, -width/2f, (int)length, (int)height, (int)width);
 	        rightWall.setRotationPoint(0f, 0f, 9f);
 	        rightWall.rotateAngleY = PI; // flip 180 deg so decal is on outside
@@ -121,20 +121,19 @@ public class ThxModelHelicopter extends ThxModel
 	        length = 16f;
 	        height = 2f;
 	        width  = 0f;
-	        tailRotor = new ModelRenderer(this, 8, 30);
+	        tailRotor = new ModelRenderer(this, 0, 2);
 	        tailRotor.addBox(-length/2f, -height/2f, -width/2f, (int)length, (int)height, (int)width);
 	        tailRotor.setRotationPoint(34f, -14f, 1.4f);
         }
 
-        tail: // will be scaled x2
+        tail:
         {
-	        length = 10f;
-	        height =  2f;
-	        width  =  1f;
-	        tail = new ModelRenderer(this, 12, 2);
+	        length = 20f;
+	        height =  4f;
+	        width  =  2f;
+	        tail = new ModelRenderer(this, 20, 26);
 	        tail.addBox(-length/2f, -height/2f, -width/2f, (int)length, (int)height, (int)width);
-	        //tail.setRotationPoint(26, -14, 0f);
-	        tail.setRotationPoint(13f, -7f, 0f); // adjusted for scale
+	        tail.setRotationPoint(26, -14, 0f);
         }
 
         rotorVerticalSupport:
@@ -142,7 +141,7 @@ public class ThxModelHelicopter extends ThxModel
 	        length = 4f;
 	        height = 22f;
 	        width = 3f;
-	        rotor2 = new ModelRenderer(this, 0, 3);
+	        rotor2 = new ModelRenderer(this, 50, 1);
 	        rotor2.addBox(-length/2f, -height/2f, -width/2f, (int)length, (int)height, (int)width);
 	        rotor2.setRotationPoint(14f, -10f, 0f);
         }
@@ -152,7 +151,7 @@ public class ThxModelHelicopter extends ThxModel
 	        length = 12;
 	        height = 2f;
 	        width = 3f;
-	        rotor3 = new ModelRenderer(this, 34, 1);
+	        rotor3 = new ModelRenderer(this, 20, 21);
 	        rotor3.addBox(-length/2f, -height/2f, -width/2f, (int)length, (int)height, (int)width);
 	        rotor3.setRotationPoint(10, -22, 0f);
         }
@@ -163,7 +162,7 @@ public class ThxModelHelicopter extends ThxModel
 	        length = 1f;
 	        height = 13f;
 	        width = 1f;
-	        cockpit1 = new ModelRenderer(this, 15, 7);
+	        cockpit1 = new ModelRenderer(this, 46, 1);
 	        cockpit1.addBox(-length/2f, -height/2f, -width/2f, (int)length, (int)height, (int)width);
 	        cockpit1.setRotationPoint(-10f, -9f, 9f);
         }
@@ -174,7 +173,7 @@ public class ThxModelHelicopter extends ThxModel
 	        length = 1f;
 	        height = 13f;
 	        width = 1f;
-	        cockpit2 = new ModelRenderer(this, 15, 7);
+	        cockpit2 = new ModelRenderer(this, 46, 1);
 	        cockpit2.addBox(-length/2f, -height/2f, -width/2f, (int)length, (int)height, (int)width);
 	        cockpit2.setRotationPoint(-10f, -9f, -9f);
         }
@@ -185,7 +184,7 @@ public class ThxModelHelicopter extends ThxModel
 	        length =  1f;
 	        height = 19f;
 	        width  =  1f;
-	        cockpit3 = new ModelRenderer(this, 15, 7);
+	        cockpit3 = new ModelRenderer(this, 46, 1);
 	        cockpit3.addBox(-length/2f, -height/2f, -width/2f, (int)length, (int)height, (int)width);
 	        cockpit3.setRotationPoint(-10f, -16f, 0f);
 	        cockpit3.rotateAngleX = PI / 2f;
@@ -216,7 +215,7 @@ public class ThxModelHelicopter extends ThxModel
         rotor2.render(scale);
         rotor3.render(scale);
         
-        tail.render(scale * 2f);
+        tail.render(scale);
         
         if (ENABLE_ROTOR)
         {
